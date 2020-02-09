be8cf26c95b4d5cf4b6709623607286364f0ae90
Cyriacus Ifeanyi <cyriacusifeanyi@gmail.com>
2020-02-09 08:41:27 +0100

    Used Spring Initializr to bootstrap this application with Web and JDBC API dependencies

---
+++ src/main/java/com/vgg/challange1/Web/API/WebApiApplication.java
@@ -0,0 +1,13 @@
+package com.vgg.challange1.Web.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiApplication.class, args);
+	}
+
+}
